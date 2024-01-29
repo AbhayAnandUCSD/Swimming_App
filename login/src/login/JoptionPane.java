3d56f8c2189104438c5e1d44ac5bba24019e152a
AbhayAnandUCSD <158004164+AbhayAnandUCSD@users.noreply.github.com>
2024-01-28 17:48:31 -0800

    Add files via upload

---
+++ login/src/login/JoptionPane.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package login;
+
+/**
+ *
+ * @author my
+ */
+class JoptionPane {
+
+    static void showMessageDialog(loginpage aThis, String username_password) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static void showMessageDialog(loginpage aThis, String username, String password) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
